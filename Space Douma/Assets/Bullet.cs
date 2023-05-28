c25e3fbeb047fdbe71f93c75b6e6544e635e29a0
Jocelyn <JocelynIsNezuko@proton.me>
2023-05-28 13:39:50 -0400

    sstill step 31
    MWHAHAHAHAHAHAHAHA

--- Space Douma/Assets/Bullet.cs
+++ Space Douma/Assets/Bullet.cs
@@ -8,15 +8,9 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(Vector2.up * Time.deltaTime * speed);
-        }
 
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector2.down * Time.deltaTime * speed);
-        }
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+
     }
 }
