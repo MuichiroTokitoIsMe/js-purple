a14cc08b2faa6be1f0dbf3b5bfede2fc0a5c00e6
ilovetapwaterr <132835392+CNMarkham@users.noreply.github.com>
2023-06-25 15:59:47 -0400

    71
    71

--- Asteroids Shinobu/Assets/Bullet.cs
+++ Asteroids Shinobu/Assets/Bullet.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-{
+    {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(transform.right * Time.deltaTime * speed);
     }
 }
