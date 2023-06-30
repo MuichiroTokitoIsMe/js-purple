379a487073490cc918d4a6f67bb53fb82cf61358
TapWater <markhamonca@codeninjas.com>
2023-06-30 17:55:28 -0400

    Step 17 for pac man
    :0

--- Asteroids Shinobu/Assets/GameManager.cs
+++ Asteroids Shinobu/Assets/GameManager.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject rockPrefab;
+    public Transform[] spawnpoints;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRocks", 3f, 10f);
     }
-    
-    
-    
     private void SpawnRocks()
     {
-        
+        //starts at 0 and stops when reaches 4, so 4 loops in total
         for (int i = 0; i < 4; i++)
-        { 
-        Vector3 randomPosition = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), 0f);
-        Instantiate(rockPrefab, randomPosition,  Quaternion.identity);
+        {
+            Vector3 randomPosition = spawnpoints[Random.Range(0, 3)].position;
+            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
+        }
     }
 }
 
-   
-}
